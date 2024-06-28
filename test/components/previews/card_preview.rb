9ca565396519cece954c8a9422011d66740935d4
Guillaume CABANEL <guillaume@kuartz.fr>
2024-06-28 14:22:07 +0200

    feat: add card component

---
+++ test/components/previews/card_preview.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class CardPreview < VitrailPreview
+  def standard
+    vt_card { "Simple card" }
+  end
+
+  def with_title
+    render_with_template
+  end
+
+  def with_description
+    render_with_template
+  end
+
+  def with_footer
+    render_with_template
+  end
+
+  def with_custom_classes
+    vt_card(class: "bg-red-100 border-red-200 text-red-800") { "Something went wrong" }
+  end
+end
